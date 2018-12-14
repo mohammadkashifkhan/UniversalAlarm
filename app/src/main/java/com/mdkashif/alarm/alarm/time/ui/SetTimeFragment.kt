04d97f5486e9d70c87abce2cfb53b7e58371ec9b
mohammadkashifkhan <mohammadkshf2093@gmail.com>
2018-12-14 19:04:34 +0530

    Interim Code added, bug fixes

--- app/src/main/java/com/mdkashif/alarm/alarm/time/ui/SetTimeFragment.kt
+++ app/src/main/java/com/mdkashif/alarm/alarm/time/ui/SetTimeFragment.kt
@@ -1,58 +1,29 @@
 package com.mdkashif.alarm.alarm.time.ui
 
+import android.app.TimePickerDialog
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.mdkashif.alarm.R
 import com.mdkashif.alarm.activities.ContainerActivity
-import com.mdkashif.alarm.alarm.time.misc.*
-import kotlinx.android.synthetic.main.fragment_add_time.*
 import kotlinx.android.synthetic.main.fragment_add_time.view.*
+import java.util.*
 
 
-class SetTimeFragment : Fragment(), TimePresenter.TimePresenterCallback {
+class SetTimeFragment : Fragment(), View.OnClickListener{
+
+
     private lateinit var mActivity: ContainerActivity
-    private var hours: List<String>? = null
-    private var days: List<String>? = null
-    private var minutes: List<String>? = null
-    private var type: List<String>? = null
+    private lateinit var rootView:View
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        var view: View =inflater.inflate(R.layout.fragment_add_time, container, false)
-        TimePresenter(this).setDataForTimeAndDays()
+        rootView = inflater.inflate(R.layout.fragment_add_time, container, false)
 
-        view.hourWheel.setOnWheelItemSelectedListener { parent, itemDrawable, position ->
-            Toast.makeText(activity, hours?.get(position)+"", Toast.LENGTH_LONG).show()
-        }
-
-        view.minuteWheel.setOnWheelItemSelectedListener { parent, itemDrawable, position ->
-            Toast.makeText(activity, minutes?.get(position)+"", Toast.LENGTH_LONG).show()
-        }
-
-        view.typeWheel.setOnWheelItemSelectedListener { parent, itemDrawable, position ->
-            Toast.makeText(activity, type?.get(position)+"", Toast.LENGTH_LONG).show()
-        }
-
-        view.btSetAlarm.setOnClickListener{
-            // TODO: insert in Db
-            //RoomHelper.transactAsync(AppDatabase.getAppDatabase(activity))
-        }
-
-        return view
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setDayslist(days)
-        setHour(hours)
-        setMinute(minutes)
-        setTimeType(type)
+        return rootView
     }
 
     override fun onAttach(context: Context?) {
@@ -60,28 +31,23 @@ class SetTimeFragment : Fragment(), TimePresenter.TimePresenterCallback {
         this.mActivity = context as ContainerActivity
     }
 
-    override fun onDaysOfWeekSuccess(hours: List<String>?, minutes: List<String>?, type: List<String>?, days: List<String>?) {
-        this.days =days
-        this.hours =hours
-        this.minutes =minutes
-        this.type =type
-    }
+    override fun onClick(p0: View?) {
+        when(p0!!.id){
+            R.id.fbAddNote->{
+                rootView.fbAddNote.visibility=View.GONE
+                rootView.etNote.visibility=View.VISIBLE
+            }
 
-    fun setDayslist(days: List<String>?){
-        rvDays.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-        rvDays.adapter = DaysInWeekAdapter(days)
-    }
-
-    fun setHour(hours: List<String>?){
-        hourWheel.adapter = HoursInDayAdapter(hours,activity)
-    }
-
-    fun setMinute(minutes: List<String>?){
-        minuteWheel.adapter = MinutesInHourAdapter(minutes,activity)
-    }
-
-    fun setTimeType(type: List<String>?){
-        typeWheel.adapter = TimeTypeInDayAdapter(type,activity)
+            R.id.tvTime->{
+                val mcurrentTime = Calendar.getInstance()
+                val hour = mcurrentTime.get(Calendar.HOUR_OF_DAY)
+                val minute = mcurrentTime.get(Calendar.MINUTE)
+                val mTimePicker: TimePickerDialog
+                mTimePicker = TimePickerDialog(mActivity, TimePickerDialog.OnTimeSetListener { timePicker, selectedHour, selectedMinute -> rootView.tvTime.text = selectedHour.toString() + ":" + selectedMinute }, hour, minute, false)
+                mTimePicker.setTitle("Select Time")
+                mTimePicker.show()
+            }
+        }
     }
 
 }
